b2b1be2c44fac3a64e5955943e80c9d3406db5b1
BestPoovarasan <poovarasan.official@gmail.com>
2022-07-30 22:08:01 +0530

    crm

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
 const dotenv = require("dotenv").config();
-const URL = process.env.DB || "mongodb+srv://crm:crm123456@cluster0.0z3n2mj.mongodb.net/?retryWrites=true&w=majority";
+const URL = process.env.DB || "mongodb+srv://crm:crm123456@cluster0.0z3n2mj.mongodb.net/blog?retryWrites=true&w=majority";
 const bcryptjs = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
