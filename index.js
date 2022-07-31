017343024f45d8a2bc9834a35964a5d55c76f8a6
BestPoovarasan <poovarasan.official@gmail.com>
2022-07-31 10:11:45 +0530

    crm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
 const dotenv = require("dotenv").config;
 const URL = process.env.DB;
-// const bcrypt = require("bcryptjs");
+const bcryptjs = require("bcrypt");
 // const jwt = require("jsonwebtoken");
 
 // Middleweare
@@ -78,36 +78,36 @@ app.post("/register", async function (req, res) {
   }
 });
 
-// app.post("/login", async function (req, res) {
-//   try {
-//     // Open the Connection
-//     const connection = await mongoClient.connect(URL);
-//     // Select the DB
-//     const db = connection.db("blog");
-//     // Select the Collection
-//     const user = await db.collection("users").findOne({ email: req.body.email });
-//     if (user) {
-//       const match = await bcrypt.compare(req.body.pass, user.pass);
-//       if (match) {
-//         // Token
-//         // const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "1m" });
-//         res.json({
-//           message: "Successfully Logged In",
-//           token,
-//         });
-//       } else {
-//         res.status(401).json({
-//           message: "Password is incorrect",
-//         });
-//       }
-//     } else {
-//       res.status(401).json({
-//         message: "User not found",
-//       });
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+app.post("/login", async function (req, res) {
+  try {
+    // Open the Connection
+    const connection = await mongoClient.connect(URL);
+    // Select the DB
+    const db = connection.db("blog");
+    // Select the Collection
+    const user = await db.collection("users").findOne({ email: req.body.email });
+    if (user) {
+      const match = await bcryptjs.compare(req.body.pass, user.pass);
+      if (match) {
+        // Token
+        // const token = jwt.sign({ _id: user._id }, process.env.SECRET, { expiresIn: "1m" });
+        res.json({
+          message: "Successfully Logged In",
+          token,
+        });
+      } else {
+        res.status(401).json({
+          message: "Password is incorrect",
+        });
+      }
+    } else {
+      res.status(401).json({
+        message: "User not found",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(process.env.PORT || 3001);
